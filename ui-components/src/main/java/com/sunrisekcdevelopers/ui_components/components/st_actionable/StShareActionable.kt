863e1e797bb67454b2a0c8cfdaacf8f7f7d19ee0
KaiCilliers <kaijohan428@gmail.com>
2022-02-14 19:32:14 +0100

    st_search_bar :: an idea for sharing a user's watchlist (playlist)

--- ui-components/src/main/java/com/sunrisekcdevelopers/ui_components/components/st_actionable/StShareActionable.kt
+++ ui-components/src/main/java/com/sunrisekcdevelopers/ui_components/components/st_actionable/StShareActionable.kt
@@ -5,6 +5,13 @@ import android.util.AttributeSet
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
 
+// idea
+// share your playlist to another user via whatever medium like messenger
+// if user has this app they can open the shared link
+// be directed to showtracker - a dedicated screen to showcase the other user's playlist
+// you can then select which ones to add to your own
+// or add the entire playlist into one of your playlist slots
+// limited to 4 or something for free accounts - paid gets more or unlimited
 class StShareActionable @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
